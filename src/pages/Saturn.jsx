d83f71868043ab35155fc2f8f2eb55d231820146
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-29 16:56:16 +0200

    Pages working with data

--- src/pages/Saturn.jsx
+++ src/pages/Saturn.jsx
@@ -1,34 +1,83 @@
+import { useState, useEffect } from "react";
+import planetData from "../data/data.json";
 export default function Saturn() {
+  const [curTab, setCurTab] = useState("overview");
+  const [pageData, setPageData] = useState({
+    name: "Saturn",
+    content: "",
+    source: "",
+    image: "",
+  });
+  function handleClick(e) {
+    setCurTab(e.target.value);
+  }
+
+  useEffect(() => {
+    const infoData = planetData.find(
+      (item) => item.name.toLowerCase() === pageData.name.toLowerCase()
+    );
+    const infoContent = infoData[curTab].content;
+    const infoSource = infoData[curTab].source;
+    const imageName =
+      curTab === "overview"
+        ? "planet"
+        : curTab === "structure"
+        ? "internal"
+        : curTab === "geology"
+        ? "geology"
+        : "";
+    const infoImage = infoData.images[imageName];
+    setPageData((prev) => ({
+      ...prev,
+      content: infoContent,
+      source: infoSource,
+      image: infoImage,
+    }));
+  }, [curTab, pageData.name]);
+
   return (
     <section className="main-wrapper">
       <div className="buttons-container">
-        <button className="active">
+        <button
+          onClick={handleClick}
+          value="overview"
+          style={{
+            backgroundColor: curTab === "overview" ? "var(--saturn)" : "",
+          }}
+        >
           <span>01</span> Overview
         </button>
-        <button>
+        <button
+          onClick={handleClick}
+          value="structure"
+          style={{
+            backgroundColor: curTab === "structure" ? "var(--saturn)" : "",
+          }}
+        >
           <span>02</span> Structure
         </button>
-        <button>
+        <button
+          onClick={handleClick}
+          value="geology"
+          style={{
+            backgroundColor: curTab === "geology" ? "var(--saturn)" : "",
+          }}
+        >
           <span>03</span> Surface
         </button>
       </div>
       <div className="image-container">
-        <img src="/mercury/planet-mercury.svg" alt="" />
+        <img
+          src={pageData.image || "/saturn/planet-saturn.svg"}
+          alt={`${pageData.name} ${curTab}`}
+        />
       </div>
       <div className="text-container">
-        <h1 className="uppercase">Mercury</h1>
-        <p className="info">
-          Mercury is the smallest planet in the Solar System and the closest to
-          the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest
-          of all the Sun's planets. Mercury is one of four terrestrial planets
-          in the Solar System, and is a rocky body like Earth.
-        </p>
+        <h1 className="uppercase">Saturn</h1>
+        <p className="info">{pageData.content}</p>
         <p className="source">
           <span>Source :</span>{" "}
-          <a
-            href="https://en.wikipedia.org/wiki/Mercury_(planet)"
-            target="_blank"
-          >
+          <a href={pageData.source} target="_blank" rel="noopener noreferrer">
             Wikipedia
           </a>
           <img src="/icon-source.svg" alt="" />
--- src/pages/Uranus.jsx
+++ src/pages/Uranus.jsx
@@ -1,34 +1,82 @@
+import { useState, useEffect } from "react";
+import planetData from "../data/data.json";
 export default function Uranus() {
+  const [curTab, setCurTab] = useState("overview");
+  const [pageData, setPageData] = useState({
+    name: "Uranus",
+    content: "",
+    source: "",
+    image: "",
+  });
+  function handleClick(e) {
+    setCurTab(e.target.value);
+  }
+
+  useEffect(() => {
+    const infoData = planetData.find(
+      (item) => item.name.toLowerCase() === pageData.name.toLowerCase()
+    );
+    const infoContent = infoData[curTab].content;
+    const infoSource = infoData[curTab].source;
+    const imageName =
+      curTab === "overview"
+        ? "planet"
+        : curTab === "structure"
+        ? "internal"
+        : curTab === "geology"
+        ? "geology"
+        : "";
+    const infoImage = infoData.images[imageName];
+    setPageData((prev) => ({
+      ...prev,
+      content: infoContent,
+      source: infoSource,
+      image: infoImage,
+    }));
+  }, [curTab, pageData.name]);
   return (
     <section className="main-wrapper">
       <div className="buttons-container">
-        <button className="active">
+        <button
+          onClick={handleClick}
+          value="overview"
+          style={{
+            backgroundColor: curTab === "overview" ? "var(--uranus)" : "",
+          }}
+        >
           <span>01</span> Overview
         </button>
-        <button>
+        <button
+          onClick={handleClick}
+          value="structure"
+          style={{
+            backgroundColor: curTab === "structure" ? "var(--uranus)" : "",
+          }}
+        >
           <span>02</span> Structure
         </button>
-        <button>
+        <button
+          onClick={handleClick}
+          value="geology"
+          style={{
+            backgroundColor: curTab === "geology" ? "var(--uranus)" : "",
+          }}
+        >
           <span>03</span> Surface
         </button>
       </div>
       <div className="image-container">
-        <img src="/mercury/planet-mercury.svg" alt="" />
+        <img
+          src={pageData.image || "/uranus/planet-uranus.svg"}
+          alt={`${pageData.name} ${curTab}`}
+        />
       </div>
       <div className="text-container">
-        <h1 className="uppercase">Mercury</h1>
-        <p className="info">
-          Mercury is the smallest planet in the Solar System and the closest to
-          the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest
-          of all the Sun's planets. Mercury is one of four terrestrial planets
-          in the Solar System, and is a rocky body like Earth.
-        </p>
+        <h1 className="uppercase">Uranus</h1>
+        <p className="info">{pageData.content}</p>
         <p className="source">
           <span>Source :</span>{" "}
-          <a
-            href="https://en.wikipedia.org/wiki/Mercury_(planet)"
-            target="_blank"
-          >
+          <a href={pageData.source} target="_blank" rel="noopener noreferrer">
             Wikipedia
           </a>
           <img src="/icon-source.svg" alt="" />
