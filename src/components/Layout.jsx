cf39156a79e8f5084be6406d489d863dce454946
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-29 08:25:09 +0200

    Body layout desktop

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
+import Footer from "./Footer";
 export default function Layout() {
   return (
     <div className="site-wrapper">
@@ -7,6 +8,7 @@ export default function Layout() {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
